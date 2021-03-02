d1027371921d613becaf06686b4055e9af640662
sejfz <eliashh97@hotmail.com>
2021-03-02 16:09:57 +0100

    more updates

--- public/radarchart/teamRadarChart.js
+++ public/radarchart/teamRadarChart.js
@@ -46,44 +46,94 @@ d3.csv("../player-stats.csv", function(data) {
         playerName.innerHTML = data[i].Player
         playerName.className = "playerName"
         playerName.id = i
-        playerName.onclick = function() { selectplayer(playerName.id) };
+        playerName.onclick = function() { selectplayer(parseInt(playerName.id)) };
         playerSelect.appendChild(playerName)
         data[i].id = i;
     }
     playerInfoArray = data
     
-    return RadarChart("#radarChart", outputData, radarChartOptions);
+    RadarChart("#radarChart", outputData, radarChartOptions);
+    outputData.splice(0, 1)
+    colourArray.splice(0, 1)
 })
 
 function selectplayer(id) {
+    for (let i=0; i<selectedPlayers.length; i++) {
+        if (selectedPlayers[i].id === id) {
+            return
+        }
+    }
     var temp = document.getElementById("playerCardTemplate");
     let playerColour = getRandomColor()
     var clon = temp.content.cloneNode(true);
+    playerInfoArray[id].playerColour = playerColour
+    clon.querySelector(".playerCard").id = "player_" + id
     clon.querySelector(".playerName").textContent = playerInfoArray[id].Player
-    clon.querySelector("#playerTeam").textContent = playerInfoArray[id].Team
-    clon.querySelector("#playerPos").textContent = playerInfoArray[id].Pos
+    clon.querySelector(".playerTeam").textContent = playerInfoArray[id].Team
+    clon.querySelector(".playerPos").textContent = playerInfoArray[id].Pos
     clon.querySelector(".playerColour").style.backgroundColor = playerColour
-    clon.querySelector(".delete").onclick = function() { removePlayer(id) };
+    clon.querySelector(".delete").onclick = function() { removePlayer(parseInt(id)) };
 
     document.getElementById("playerProfiles").appendChild(clon);
-    colourArray.push(playerColour)
-    selectedPlayers.push(playerInfoArray[id])
-    updatePlayer(id)
+    addPlayer(id)
 }
 
-function updatePlayer(playerId) {
-    let tempArr = [
-        {axis: "Win rate", value: parseFloat(playerInfoArray[playerId]["W%"])},
-        {axis: "First blood rate", value: parseFloat(playerInfoArray[playerId]["FB%"])},
-        {axis: "Kill participation", value: parseFloat(playerInfoArray[playerId]["KP"])},
-        {axis: "Average share of team death", value: parseFloat(playerInfoArray[playerId]["DTH%"])},
-        {axis: "Gold share", value: parseFloat(playerInfoArray[playerId]["GOLD%"])},
-        {axis: "Damage", value: parseFloat(playerInfoArray[playerId]['DMG%'])}
-    ]
-    outputData.push(tempArr)
+function addPlayer(playerId) {
+    selectedPlayers.push(playerInfoArray[playerId])
+    colourArray.push(playerInfoArray[playerId].playerColour)
+    updatePlayer()
+}
+
+function updatePlayer() {
+    outputData = []
+    //colourArray = []
+    
+    for (let i=0; i<selectedPlayers.length; i++) {
+        // colourArray.push(selectedPlayers[i].playerColour)
+        // console.log(colourArray)   
+        let tempArr = [
+            {axis: "Win rate", value: parseFloat(selectedPlayers[i]["W%"])},
+            {axis: "First blood rate", value: parseFloat(selectedPlayers[i]["FB%"])},
+            {axis: "Kill participation", value: parseFloat(selectedPlayers[i]["KP"])},
+            {axis: "Average share of team death", value: parseFloat(selectedPlayers[i]["DTH%"])},
+            {axis: "Gold share", value: parseFloat(selectedPlayers[i]["GOLD%"])},
+            {axis: "Damage", value: parseFloat(selectedPlayers[i]['DMG%'])}
+        ]
+        outputData.push(tempArr)
+    }
+    //console.log(radarChartOptions.color)
     return RadarChart("#radarChart", outputData, radarChartOptions);
 }
 
 function removePlayer(id) {
-    //for (let i=0; i<)
+    let removePlayerId = null;
+    for (let i=0; i<selectedPlayers.length; i++) {
+        console.log({play: selectedPlayers[i].id, id: id})
+        if (selectedPlayers[i].id === id) {
+            
+            for (let j=0; j<colourArray.length; j++) {
+                if (selectedPlayers[i].playerColour === colourArray[j]) {
+                    colourArray.splice(j, 1)
+                }
+            }
+            selectedPlayers.splice(i, 1)
+        }
+    }
+    
+    document.getElementById("player_" + id).remove()
+    if (selectedPlayers.length<1) {
+        outputData = [[
+            { axis: "Win rate", value: 0 },
+            { axis: "First blood rate", value: 0 },
+            { axis: "Kill participation", value: 0},
+            { axis: "Average share of team death", value: 0 },
+            { axis: "Gold share", value: 0 },
+            { axis: "Damage", value: 0 }
+        ]]
+        RadarChart("#radarChart", outputData, radarChartOptions);
+    } else {
+        updatePlayer()
+    }
+    
+
 }
