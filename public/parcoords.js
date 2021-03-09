e534d968da5d7f5bdeb03720a79037d866a3c4eb
sejfz <eliashh97@hotmail.com>
2021-03-09 14:55:54 +0100

    tja

---
+++ public/parcoords.js
@@ -0,0 +1,478 @@
+var margin = {top: 66, right: 110, bottom: 20, left: 188},
+    width = document.body.clientWidth - margin.left - margin.right - 10, //Vad ska man ha här
+    height = 460 - margin.top - margin.bottom,
+    innerHeight = height - 2;
+
+var devicePixelRatio = window.devicePixelRatio || 1;
+
+var color = d3.scaleOrdinal()
+  .range(["#5DA5B3","#D58323","#DD6CA7","#54AF52","#8C92E8","#E15E5A","#725D82","#776327","#50AB84","#954D56","#AB9C27","#517C3F","#9D5130","#357468","#5E9ACF","#C47DCB","#7D9E33","#DB7F85","#BA89AD","#4C6C86","#B59248","#D8597D","#944F7E","#D67D4B","#8F86C2"]);
+
+/*var color = function getRandomColor() { // generates a random colour when a player is selected
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}*/
+var types = {
+  "Number": {
+    key: "Number",
+    coerce: function(d) {
+      return +parseFloat(d); 
+    },
+    extent: d3.extent,
+    within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+    defaultScale: d3.scaleLinear().range([innerHeight, 0])
+  },
+  "String": {
+    key: "String",
+    coerce: String,
+    extent: function (data) { return data.sort(); },
+    within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+    defaultScale: d3.scalePoint().range([0, innerHeight])
+  },
+  "Date": {
+    key: "Date",
+    coerce: function(d) { return new Date(d); },
+    extent: d3.extent,
+    within: function(d, extent, dim) { return extent[0] <= dim.scale(d) && dim.scale(d) <= extent[1]; },
+    defaultScale: d3.scaleTime().range([0, innerHeight])
+  }
+};
+
+var dimensions = [
+  {
+    key: "Team",
+    description: "Teams",
+    type: types["String"],
+    axis: d3.axisLeft()
+      .tickFormat(function(d,i) {
+        return d;
+      })
+  },
+  
+  {
+    key: "GP",
+    description: "Games played",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "W%",
+    description: "Win percentage",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "CTR%",
+    description: "Counter pick rate",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "K",
+    description: "Total kills",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "D",
+    description: "Total deaths",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "A",
+    description: "Total assists",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "KDA",
+    description: "Kill/death/assist ratio",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "KP",
+    description: "Kill participation",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "KS%",
+    description: "Kill share",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "DTH%",
+    description: "Average share of team's deaths",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "FB%",
+    description: "First blood rate",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "GD10",
+    description: "Average gold diff at 10 min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "XPD10",
+    description: "Average exp diff at 10 min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "CSD10",
+    description: "Average cs score diff at 10 min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "CSPM",
+    description: "Average cs score per min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "CS%P15",
+    description: "Average share of team's total cs > 15 min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "DPM",
+    description: "Average dmg to champs per min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "DMG%",
+    description: "Average share of team's total dmg to champs",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "EGPM",
+    description: "Average gold earned per min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "GOLD%",
+    description: "Average share of team's total gold earned",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "WPM",
+    description: "Average wards placed per min",
+    type: types["Number"],
+    scale: d3.scaleSqrt().range([innerHeight, 0])
+  },
+  {
+    key: "WCPM",
+    description: "Average wards cleared per min",
+    type: types["String"],
+    axis: d3.axisRight()
+      .tickFormat(function(d,i) {
+        if (d == null) return "(null)";
+        return i % 5 == 0 ? d.slice(0,22) : "";
+      })
+  }
+];
+
+var xscale = d3.scalePoint()
+    .domain(d3.range(dimensions.length))
+    .range([0, width]);
+
+var yAxis = d3.axisLeft();
+
+var container = d3.select(".page").append("div")
+    .attr("class", "parcoords")
+    .style("width", width + margin.left + margin.right + "px")
+    .style("height", height + margin.top + margin.bottom + "px");
+
+var svg = container.append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+var canvas = container.append("canvas")
+    .attr("width", width * devicePixelRatio)
+    .attr("height", height * devicePixelRatio)
+    .style("width", width + "px")
+    .style("height", height + "px")
+    .style("margin-top", margin.top + "px")
+    .style("margin-left", margin.left + "px");
+
+var ctx = canvas.node().getContext("2d");
+ctx.globalCompositeOperation = 'darken';
+ctx.globalAlpha = 0.15;
+ctx.lineWidth = 1.5;
+ctx.scale(devicePixelRatio, devicePixelRatio);
+
+var output = d3.select(".page").append("pre");
+
+var axes = svg.selectAll(".axis")
+    .data(dimensions)
+    .enter().append("g")
+    .attr("class", function(d) { return "axis " + d.key.replace(/ /g, "_"); })
+    .attr("transform", function(d,i) { return "translate(" + xscale(i) + ")"; });
+    
+
+d3.csv("player-stats.csv", function(error, data) {
+  if (error) throw error;
+
+  // shuffle the data!
+  data = d3.shuffle(data);
+  data.forEach(function(d) {
+    dimensions.forEach(function(p) {
+      d[p.key] = !d[p.key] ? null : p.type.coerce(d[p.key]);
+    });
+
+    // truncate long text strings to fit in data table
+    for (var key in d) {
+      if (d[key] && d[key].length > 35) d[key] = d[key].slice(0,36);
+    }
+  });
+
+  // type/dimension default setting happens here
+  dimensions.forEach(function(dim) {
+    if (!("domain" in dim)) {
+      // detect domain using dimension type's extent function
+      dim.domain = d3_functor(dim.type.extent)(data.map(function(d) { return d[dim.key]; }));
+    }
+    if (!("scale" in dim)) {
+      // use type's default scale for dimension
+      dim.scale = dim.type.defaultScale.copy();
+    }
+    dim.scale.domain(dim.domain);
+  });
+
+  var render = renderQueue(draw).rate(50);
+
+  ctx.clearRect(0,0,width,height);
+  ctx.globalAlpha = d3.min([0.85/Math.pow(data.length,0.3),1]);
+  render(data);
+
+  axes.append("g")
+      .each(function(d) {
+        var renderAxis = "axis" in d
+          ? d.axis.scale(d.scale)  // custom axis
+          : yAxis.scale(d.scale);  // default axis
+        d3.select(this).call(renderAxis);
+      })
+    .append("text")
+      .attr("class", "title")
+      .attr("text-anchor", "start")
+      .text(function(d) { return "description" in d ? d.description : d.key; });
+
+  // Add and store a brush for each axis.
+  axes.append("g")
+      .attr("class", "brush")
+      .each(function(d) {
+        d3.select(this).call(d.brush = d3.brushY()
+          .extent([[-10,0], [10,height]])
+          .on("start", brushstart)
+          .on("brush", brush)
+          .on("end", brush)
+        )
+      })
+    .selectAll("rect")
+      .attr("x", -8)
+      .attr("width", 16);
+
+  d3.selectAll(".axis.Team .tick text")
+    .style("fill", color);
+  //console.log(Object.keys(data.slice(0,24)[0]))
+  for (let i=0; i<data.slice(0,24).length; i++) {
+    var temp = document.getElementById("tableRowTemplate");
+    var clon = temp.content.cloneNode(true);
+
+    clon.querySelector(".Player").textContent = data.slice(0,24)[i].Player
+    clon.querySelector(".Team").textContent = data.slice(0,24)[i].Team
+    clon.querySelector(".Pos").textContent = data.slice(0,24)[i].Pos
+    clon.querySelector(".GP").textContent = data.slice(0,24)[i].GP
+    clon.querySelector(".W").textContent = data.slice(0,24)[i]["W%"]
+    clon.querySelector(".CTR").textContent = data.slice(0,24)[i]["CTR%"]
+    clon.querySelector(".K").textContent = data.slice(0,24)[i].K
+    clon.querySelector(".D").textContent = data.slice(0,24)[i].D
+    clon.querySelector(".A").textContent = data.slice(0,24)[i].A
+    clon.querySelector(".KDA").textContent = data.slice(0,24)[i].KDA
+    clon.querySelector(".KP").textContent = data.slice(0,24)[i].KP
+    clon.querySelector(".KS").textContent = data.slice(0,24)[i]["KS%"]
+    clon.querySelector(".DTH").textContent = data.slice(0,24)[i]["DTH%"]
+    clon.querySelector(".FB").textContent = data.slice(0,24)[i]["FB%"]
+    clon.querySelector(".GD10").textContent = data.slice(0,24)[i].GD10
+    clon.querySelector(".XPD10").textContent = data.slice(0,24)[i].XPD10
+    clon.querySelector(".CSD10").textContent = data.slice(0,24)[i].CSD10
+    clon.querySelector(".CSPM").textContent = data.slice(0,24)[i].CSPM
+    clon.querySelector(".CSP15").textContent = data.slice(0,24)[i]["CS%P15"]
+    clon.querySelector(".DPM").textContent = data.slice(0,24)[i].DPM
+    clon.querySelector(".DMG").textContent = data.slice(0,24)[i]["DMG%"]
+    clon.querySelector(".EGPM").textContent = data.slice(0,24)[i].EGPM
+    clon.querySelector(".GOLD").textContent = data.slice(0,24)[i]["GOLD%"]
+    clon.querySelector(".WPM").textContent = data.slice(0,24)[i].WPM
+    clon.querySelector(".WCPM").textContent = data.slice(0,24)[i].WCPM
+
+    document.getElementById("tableBody").appendChild(clon);
+  }
+  function project(d) {
+    return dimensions.map(function(p,i) {
+      // check if data element has property and contains a value
+      if (
+        !(p.key in d) ||
+        d[p.key] === null
+      ) return null;
+
+      return [xscale(i),p.scale(d[p.key])];
+    });
+  };
+
+  function draw(d) {
+    ctx.strokeStyle = color(d.Team);
+    ctx.beginPath();
+    var coords = project(d);
+    coords.forEach(function(p,i) {
+      // this tricky bit avoids rendering null values as 0
+      if (p === null) {
+        // this bit renders horizontal lines on the previous/next
+        // dimensions, so that sandwiched null values are visible
+        if (i > 0) {
+          var prev = coords[i-1];
+          if (prev !== null) {
+            ctx.moveTo(prev[0],prev[1]);
+            ctx.lineTo(prev[0]+6,prev[1]);
+          }
+        }
+        if (i < coords.length-1) {
+          var next = coords[i+1];
+          if (next !== null) {
+            ctx.moveTo(next[0]-6,next[1]);
+          }
+        }
+        return;
+      }
+      
+      if (i == 0) {
+        ctx.moveTo(p[0],p[1]);
+        return;
+      }
+
+      ctx.lineTo(p[0],p[1]);
+    });
+    ctx.stroke();
+  }
+
+  function brushstart() {
+    d3.event.sourceEvent.stopPropagation();
+  }
+
+  // Handles a brush event, toggling the display of foreground lines.
+  function brush() {
+    render.invalidate();
+
+    var actives = [];
+    svg.selectAll(".axis .brush")
+      .filter(function(d) {
+        return d3.brushSelection(this);
+      })
+      .each(function(d) {
+        actives.push({
+          dimension: d,
+          extent: d3.brushSelection(this)
+        });
+      });
+
+    var selected = data.filter(function(d) {
+      if (actives.every(function(active) {
+          var dim = active.dimension;
+          // test if point is within extents for each active brush
+          return dim.type.within(d[dim.key], active.extent, dim);
+        })) {
+        return true;
+      }
+    });
+
+    // show ticks for active brush dimensions
+    // and filter ticks to only those within brush extents
+    /*
+    svg.selectAll(".axis")
+        .filter(function(d) {
+          return actives.indexOf(d) > -1 ? true : false;
+        })
+        .classed("active", true)
+        .each(function(dimension, i) {
+          var extent = extents[i];
+          d3.select(this)
+            .selectAll(".tick text")
+            .style("display", function(d) {
+              var value = dimension.type.coerce(d);
+              return dimension.type.within(value, extent, dimension) ? null : "none";
+            });
+        });
+
+    // reset dimensions without active brushes
+    svg.selectAll(".axis")
+        .filter(function(d) {
+          return actives.indexOf(d) > -1 ? false : true;
+        })
+        .classed("active", false)
+        .selectAll(".tick text")
+          .style("display", null);
+    */
+
+    ctx.clearRect(0,0,width,height);
+    ctx.globalAlpha = d3.min([0.85/Math.pow(selected.length,0.3),1]);
+    render(selected);
+    document.getElementById("tableBody").innerHTML= ''
+    for (let i=0; i<selected.slice(0,24).length; i++) {
+      var temp2 = document.getElementById("tableRowTemplate");
+      var clon = temp2.content.cloneNode(true);
+  
+      clon.querySelector(".Player").textContent = selected.slice(0,24)[i].Player
+      clon.querySelector(".Team").textContent = selected.slice(0,24)[i].Team
+      clon.querySelector(".Pos").textContent = selected.slice(0,24)[i].Pos
+      clon.querySelector(".GP").textContent = selected.slice(0,24)[i].GP
+      clon.querySelector(".W").textContent = selected.slice(0,24)[i]["W%"]
+      clon.querySelector(".CTR").textContent = selected.slice(0,24)[i]["CTR%"]
+      clon.querySelector(".K").textContent = selected.slice(0,24)[i].K
+      clon.querySelector(".D").textContent = selected.slice(0,24)[i].D
+      clon.querySelector(".A").textContent = selected.slice(0,24)[i].A
+      clon.querySelector(".KDA").textContent = selected.slice(0,24)[i].KDA
+      clon.querySelector(".KP").textContent = selected.slice(0,24)[i].KP
+      clon.querySelector(".KS").textContent = selected.slice(0,24)[i]["KS%"]
+      clon.querySelector(".DTH").textContent = selected.slice(0,24)[i]["DTH%"]
+      clon.querySelector(".FB").textContent = selected.slice(0,24)[i]["FB%"]
+      clon.querySelector(".GD10").textContent = selected.slice(0,24)[i].GD10
+      clon.querySelector(".XPD10").textContent = selected.slice(0,24)[i].XPD10
+      clon.querySelector(".CSD10").textContent = selected.slice(0,24)[i].CSD10
+      clon.querySelector(".CSPM").textContent = selected.slice(0,24)[i].CSPM
+      clon.querySelector(".CSP15").textContent = selected.slice(0,24)[i]["CS%P15"]
+      clon.querySelector(".DPM").textContent = selected.slice(0,24)[i].DPM
+      clon.querySelector(".DMG").textContent = selected.slice(0,24)[i]["DMG%"]
+      clon.querySelector(".EGPM").textContent = selected.slice(0,24)[i].EGPM
+      clon.querySelector(".GOLD").textContent = selected.slice(0,24)[i]["GOLD%"]
+      clon.querySelector(".WPM").textContent = selected.slice(0,24)[i].WPM
+      clon.querySelector(".WCPM").textContent = selected.slice(0,24)[i].WCPM
+  
+      document.getElementById("tableBody").appendChild(clon);
+    }
+    //output.text(d3.tsvFormat(selected.slice(0,24)));
+  }
+});
+
+function d3_functor(v) {
+  return typeof v === "function" ? v : function() { return v; };
+};
